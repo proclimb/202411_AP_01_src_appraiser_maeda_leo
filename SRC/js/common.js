d839f3b9ef8e0b6062d63b6c061e47f0b9e1fc74
LeoMaeda <202411ap01_maeda@x-ide.sakura.ne.jp>
2025-02-27 15:32:03 +0900

    Redmineのチケットナンバー　#36359
    ・不具合状況の説明 ・正常系にも関わらずエラーメッセージが表示され、4桁整数の場合にエラーメッセージが表示されない
    ・届出用紙欄や現地調査欄の不具合
    修正内容の説明
    ・Common.js、Const.jsでのスクリプト修正実施

--- SRC/js/common.js
+++ SRC/js/common.js
@@ -7,9 +7,7 @@
 function fnYMDCheck(msg, obj) {
 	// 未入力時はチェックしない
 	oYMD = obj.value;
-	if (!oYMD) {
-		return true;
-	}
+	if (!oYMD) { return true; }
 
 	var tmp = oYMD.split('/');
 	var ymd = new Date(tmp[0], parseInt(tmp[1], 10) - 1, parseInt(tmp[2], 10));
